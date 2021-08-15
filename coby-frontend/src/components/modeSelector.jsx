eb1cc2317b5ed92376d9a729ea12d0afa2e61354
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-08-14 21:04:47 -0400

    added modeSelector component

---
+++ coby-frontend/src/components/modeSelector.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ModeSelector = ({ onChange, options }) => {
+    const elements = []
+
+    for (let i = 0; i < options.length; i++) {
+        elements.push(<option key={options[i]} className = "mode-selector-option" >{options[i]}</option>)
+    }
+
+    return (
+        <select onChange={(e) => {console.log(e.target.value)}} className = "mode-selector-select">
+            {elements}
+        </select>
+    )
+}
+
+export default ModeSelector
