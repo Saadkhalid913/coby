ca1b612001681157e6a9ac41bd63f35869614c22
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-08-17 19:25:52 -0400

    bug fix

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import json
 
 from werkzeug.datastructures import FileStorage
 from utils import ParseReqBody
-from imageUtils import imgToArray
-from imageUtils import Convolution3D, SaveImgFromArray, Filter3D, Greyscale
+# from imageUtils import imgToArray
+# from imageUtils import Convolution3D, SaveImgFromArray, Filter3D, Greyscale
 import numpy as np 
 import shutil
 import os 
@@ -16,30 +16,30 @@ from pathlib import Path
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/upload", methods = ["POST"])
-@cross_origin()
-def upload():
-    # try:
-        shutil.rmtree("images")
-        os.mkdir("images")
-        matrix = parseMatrix(request)
-        img = parseImage(request)
-        mode = request.form["mode"].lower()
+# @app.route("/upload", methods = ["POST"])
+# @cross_origin()
+# def upload():
+#     try:
+#         shutil.rmtree("images")
+#         os.mkdir("images")
+#         matrix = parseMatrix(request)
+#         img = parseImage(request)
+#         mode = request.form["mode"].lower()
 
-        if mode == "convolution":
-            img = Convolution3D(img, matrix)
-        elif mode == "filter":
-            img = Filter3D(img, matrix)
-        elif mode == "greyscale":
-            img = Greyscale(img)
-        else:
-            return Greyscale(img)
+#         if mode == "convolution":
+#             img = Convolution3D(img, matrix)
+#         elif mode == "filter":
+#             img = Filter3D(img, matrix)
+#         elif mode == "greyscale":
+#             img = Greyscale(img)
+#         else:
+#             return Greyscale(img)
 
-        imgpath = SaveImgFromArray(img)
-        return send_file(imgpath, mimetype="image/png", download_name="result.png")
-    # except Exception as ex:
-    #     print(ex)
-    #     return {"error": "There was an error"}
+#         imgpath = SaveImgFromArray(img)
+#         return send_file(imgpath, mimetype="image/png", download_name="result.png")
+#     except Exception as ex:
+#         print(ex)
+#         return {"error": "There was an error"}
 
 @app.route("/about", methods = ["GET"])
 def serveAbout():
@@ -72,19 +72,19 @@ def serveHome():
 
     
     
-def parseMatrix(request):
-    rawMatrix = list(map(int, request.form["matrix"].split(",")))
-    matrixSize = int( request.form["size"])
-    matrix = np.array(rawMatrix).reshape((matrixSize , -1))
-    assert matrix.shape[1] == matrix.shape[0]
-    return matrix 
+# def parseMatrix(request):
+#     rawMatrix = list(map(int, request.form["matrix"].split(",")))
+#     matrixSize = int( request.form["size"])
+#     matrix = np.array(rawMatrix).reshape((matrixSize , -1))
+#     assert matrix.shape[1] == matrix.shape[0]
+#     return matrix 
 
-def parseImage(request):
-    imageBytes = request.files["image"].read()
-    if len(imageBytes) > 10000000:
-        raise Exception()
-    img = imgToArray(imageBytes)
-    return img 
+# def parseImage(request):
+#     imageBytes = request.files["image"].read()
+#     if len(imageBytes) > 10000000:
+#         raise Exception()
+#     img = imgToArray(imageBytes)
+    # return img 
 
 if __name__ == "__main__":
     app.run()
